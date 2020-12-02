20f2d681904c0ab1800566ad94e18be7fbd68d3c
Brad Murray <brad@faire.com>
2020-12-01 20:47:37 -0500

    1 part 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -17,24 +17,43 @@ fn read_input_numbers() -> Result<Vec<i32>, io::Error> {
         .collect())
 }
 
-fn main() {
-    const TARGET_SUM: i32 = 2020;
+const TARGET_SUM: i32 = 2020;
 
-    let input_numbers= read_input_numbers();
+fn find_two(sorted_numbers: &[i32], target_sum: i32) -> Option<i32> {
+    let midpoint_index = sorted_numbers.iter().position(|n| n > &(target_sum / 2)).unwrap();
+    let lower_half = &sorted_numbers[..midpoint_index];
+    let upper_half = &sorted_numbers[midpoint_index..];
 
-    if let Ok(mut numbers) = input_numbers {
-        numbers.sort_unstable();
-
-        let midpoint_index = numbers.iter().position(|n| n > &(TARGET_SUM / 2)).unwrap();
-        let lower_half = &numbers[..midpoint_index];
-        let upper_half = &numbers[midpoint_index..];
-
-        for n in lower_half {
-            let match_index = upper_half.binary_search(&(TARGET_SUM - n));
-            if match_index.is_ok() {
-                println!("Solution: {}", upper_half[match_index.unwrap()] * n);
-                break;
-            }
+    for n in lower_half {
+        let match_index = upper_half.binary_search(&(target_sum - n));
+        if match_index.is_ok() {
+            return Some(upper_half[match_index.unwrap()] * n);
         }
     }
+
+    None
+}
+
+fn find_three(numbers: &[i32]) -> Option<i32> {
+    for index in 0..(numbers.len() - 1) {
+        let value = numbers[index];
+        let remainder = TARGET_SUM - value;
+        let result = find_two(&numbers[(index + 1)..], remainder);
+        if result.is_some() {
+            return Some(value * result.unwrap());
+        }
+    }
+
+    None
+}
+
+fn main() {
+    let mut numbers = read_input_numbers().unwrap();
+    numbers.sort_unstable();
+
+    let find_two_solution = find_two(&*numbers, TARGET_SUM);
+    println!("Find two solution: {}", find_two_solution.unwrap());
+
+    let find_three_solution = find_three(&*numbers);
+    println!("Find three solution: {}", find_three_solution.unwrap());
 }
